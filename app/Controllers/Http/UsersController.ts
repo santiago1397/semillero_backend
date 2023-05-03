e95e7e28e9e3c8043cc3fed11edd4b451d293bb4
Eloy Gonzalez <eloygonzalez240@gmail.com>
2023-05-03 10:07:11 -0400

    Actualizacion de controlador Users y configuracion de rutas globales

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,53 +1,134 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { prisma } from '@ioc:Adonis/Addons/Prisma'
+import { Users } from '@prisma/client';
+import { enumErrors, enumSuccess } from '../../Utils/utils'
 
 export default class UsersController {
   public async index({}: HttpContextContract) {
-    const users = await prisma.users.findMany({
-      take: 10,
-      skip: 0,
-      select: {
-        id: true,
-        email: true,
-        createdBy: true,
-        updatedBy: true,
-        version: true,
-        profile: {
-          select: {
-            firstName: true,
-            lastName: true
-          },
-        },
-        userRole: {
-          select: {
-            roleId: true,
-            role: {
-              select : {
-                name: true
-              }
+    try {
+      const [ total, data ] = await prisma.$transaction([
+        prisma.users.count(),
+        prisma.users.findMany({
+        take: 10,
+        skip: 0,
+        select: {
+          id: true,
+          email: true,
+          createdBy: true,
+          updatedBy: true,
+          version: true,
+          profile: {
+            select: {
+              firstName: true,
+              lastName: true
             },
-            enteId: true,
-            ente: {
-              select: {
-                name: true
+          },
+          userRole: {
+            select: {
+              roleId: true,
+              role: {
+                select : {
+                  name: true
+                }
+              },
+              enteId: true,
+              ente: {
+                select: {
+                  name: true
+                }
               }
             }
           }
-        }
-      },
-    })
-    return users;
+        },
+        })
+      ]);
+  
+      return { total, data };      
+    } catch (error) {
+      console.log(error);
+      return { message: enumErrors.DEFAULT };
+    }
   }
 
   public async create({}: HttpContextContract) {}
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request }: HttpContextContract) {
+    const data = request.body() as Users;
+    try {
+      await prisma.users.create({ data: { ...data } });
+      return { message: enumSuccess.CREATE }
+    } catch (err) {
+      console.log(err);
+      return { message: enumErrors.ERROR_CREATE };
+    }
+  }
 
-  public async show({}: HttpContextContract) {}
+  public async show({ params }: HttpContextContract) {
+    try {
+      const { id } = params;
+      const [ data ] = await prisma.$transaction([
+        prisma.users.findMany({
+          where: { id : Number(id)},
+          select: {
+            id: true,
+            email: true,
+            createdBy: true,
+            updatedBy: true,
+            version: true,
+            profile: {
+              select: {
+                firstName: true,
+                lastName: true
+              },
+            },
+            userRole: {
+              select: {
+                roleId: true,
+                role: {
+                  select : {
+                    name: true
+                  }
+                },
+                enteId: true
+              }
+            }
+          },
+        })
+      ]);
+      return { data };
+    } catch (error) {
+      console.log(error);
+      return { message: enumErrors.ERROR_SELECT }
+    }
+  }
 
   public async edit({}: HttpContextContract) {}
 
-  public async update({}: HttpContextContract) {}
+  public async update({ request, params }: HttpContextContract) {
+    try {
+      const { id } = params;
+      const data = request.body();
+      await prisma.users.update({
+        where: { id: Number(id) },
+        data: data
+      });
+      return { message: enumSuccess.UPDATE }
+    } catch (error) {
+      console.log(error);
+      return { message: enumErrors.ERROR_UPDATE }
+    }
+  }
 
-  public async destroy({}: HttpContextContract) {}
+  public async destroy({ params }: HttpContextContract) {
+    try {
+      const { id } = params;
+      await prisma.activities.delete({
+        where: { id: Number(id) }
+      });
+      return { message: enumSuccess.DELETE }
+    } catch (error) {
+      console.log(error);
+      return { message: enumErrors.ERROR_DELETE }
+    }
+  }
 }
---
+++ app/Utils/utils.ts
@@ -0,0 +1,13 @@
+export const enumErrors = {
+  DEFAULT: 'Un error ha ocurrido en el servidor',
+  ERROR_CREATE: 'Error al crear el registro',
+  ERROR_SELECT: 'Error al consultar el registro',
+  ERROR_UPDATE: 'Error al actualizar el registro',
+  ERROR_DELETE: 'Error al eliminar el registro'
+};
+
+export const enumSuccess = {
+  CREATE: 'Se ha registrado un nuevo registro',
+  UPDATE: 'Se ha actualizado el registro exitosamente',
+  DELETE: 'Se ha eliminado el registro exitosamente'
+}
