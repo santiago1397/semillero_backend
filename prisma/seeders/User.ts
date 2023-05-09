c031f59cb2ed620a9da0df666082ff37dd8f0a2c
Eloy Gonzalez <eloygonzalez240@gmail.com>
2023-05-09 17:19:22 -0400

    Fix seeder to UserRole

--- prisma/seeders/User.ts
+++ prisma/seeders/User.ts
@@ -31,6 +31,12 @@ export default class UserSeeder extends PrismaSeederBase {
             phone: '******************',
           },
         },
+        userRole: {
+          create: {
+            roleId: 3,
+            enteId: 3,
+          },
+        },
       },
       {
         email: 'admin@mppct.gob.ve',
